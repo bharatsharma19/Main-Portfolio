8882d3e49e7b5fd58c0697c9cd4de758e27a7d8f
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-06-17 20:26:37 +0530

    enter

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -12,7 +12,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#portfolio">Portfolio</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#work">Works</a>
+          <a href="#works">Works</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
           <a href="#testimonial">Testimonials</a>
